f420b0fc27729c21a58297e24fd71d6ca48e4355
Kristjanmn <kristjanmn@hotmail.com>
2025-01-24 10:15:35 +0200

    Initial commit

---
+++ src/main/java/io/nqa/test/bankaccounthandling/BankAccountHandlingApplication.java
@@ -0,0 +1,13 @@
+package io.nqa.test.bankaccounthandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountHandlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nqa/test/bankaccounthandling/BankAccountHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nqa.test.bankaccounthandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
